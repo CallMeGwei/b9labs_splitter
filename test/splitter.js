55c84b440e86de915c6f5c34be1dbae83f887685
CallMeGwei <callmegwei@gmail.com>
2019-04-23 18:30:28 -0500

    Fixed truffle tests so they use BN everywhere.

--- test/splitter.js
+++ test/splitter.js
@@ -14,37 +14,38 @@ contract("Splitter", async accounts  => {
         let aliceInitalBalance = new BN(await web3.eth.getBalance(alice));  // Alice's ethereum balance.
         let transaction = await instance.splitBetween( bob, carol, {from: alice, value: web3.utils.toWei("1", "finney")} ); // Alice sends 1 finney to the contract.
         let aliceAfterSplitBalance = new BN(await web3.eth.getBalance(alice)); // Alice's ethereum balance.
-        let gasCost = new BN(transaction.receipt.gasUsed); // The gas that was used to process the splitBetween transaction.
-
-        console.log("\nInital Balance: " + aliceInitalBalance);
-        console.log("\nGas Used: " + gasCost);
-        console.log("\nOne Finney: " + new BN(web3.utils.toWei("1", "finney")))
-        console.log("\nInital Balance minus 1 Finney minus gas: "+ aliceInitalBalance.sub(new BN(web3.utils.toWei("1", "finney"))).sub(gasCost) +"\n");
-        console.log("\nAfter Split Balance: "+ aliceAfterSplitBalance +"\n");
+        let gasUsed = (new BN(transaction.receipt.gasUsed));
+        let gasPrice = (new BN((await web3.eth.getTransaction(transaction.tx)).gasPrice));
+        let gasCost = gasUsed.mul(gasPrice); // The gas that was used to process the splitBetween transaction.
 
         assert.strictEqual(
-            aliceAfterSplitBalance, 
-            aliceInitalBalance.sub(new BN(web3.utils.toWei("1", "finney"))).sub(gasCost),
+            aliceAfterSplitBalance.toString(), // ethreum address balance after split
+            (aliceInitalBalance.sub(new BN(web3.utils.toWei("1", "finney"))).sub(gasCost)).toString(), // should equal inital balance minus transfered amount minus gs costs for transfer
             "Alice's balance did not go down by 1 finney plus gas."
         );
     });
 
     it("Bob and Carol should have equal in-contract balances of .5 finney right after the first 1 finney split.", async () => {
         let instance = await Splitter.deployed();
-        let bobContractBalance = await instance.balances(bob);
-        let carolContractBalance = await instance.balances(carol);
-        assert.equal(bobContractBalance.toString(), web3.utils.toWei(".5", "finney"), "Bob's balance is wrong.");
-        assert.equal(carolContractBalance.toString(), web3.utils.toWei(".5", "finney"), "Carol's balance is wrong.");
+        let bobContractBalance = new BN(await instance.balances(bob));
+        let carolContractBalance = new BN(await instance.balances(carol));
+        assert.strictEqual(bobContractBalance.toString(), web3.utils.toWei(".5", "finney"), "Bob's balance is wrong.");
+        assert.strictEqual(carolContractBalance.toString(), web3.utils.toWei(".5", "finney"), "Carol's balance is wrong.");
     });
 
     it("Bob should be able to withdraw his ether contract balance, his on-chain balance should increase, and his contract balance should go to zero.", async () => {
         let instance = await Splitter.deployed();
-        let bobInitialBalance = await web3.eth.getBalance(bob);
+        let bobInitialBalance = new BN(await web3.eth.getBalance(bob));
+        let bobInitialContractBalance = new BN(await instance.balances(bob));
         let transaction = await instance.withdraw( {from: bob} );
-        let bobNewContractBalance = await instance.balances(bob);
-        let bobNewBalance = await web3.eth.getBalance(bob);
-        assert.isAbove(parseInt(bobNewBalance), parseInt(bobInitialBalance), "Bob's new balance isn't greater than the initial balance.");
-        assert.equal(parseInt(bobNewContractBalance), 0, "Bob's withdrawal wasn't complete.");
+        let bobNewContractBalance = new BN(await instance.balances(bob));
+        let bobNewBalance = new BN(await web3.eth.getBalance(bob));
+        let gasUsed = new BN(transaction.receipt.gasUsed);
+        let gasPrice = new BN((await web3.eth.getTransaction(transaction.tx)).gasPrice);
+        let gasCost = gasUsed.mul(gasPrice); // The gas that was used to process the splitBetween transaction.
+
+        assert.strictEqual(bobNewBalance.toString(), (bobInitialBalance.add(bobInitialContractBalance).sub(gasCost)).toString(), "Bob's new balance is not equal to initial balance, plus withdraw, minus gas.");
+        assert.strictEqual(bobNewContractBalance.toString(), "0", "Bob's withdrawal wasn't complete.");
     });
 
     it("Should revert for an address attempting withdrawal that has a zero in-contract balance.", async () => {

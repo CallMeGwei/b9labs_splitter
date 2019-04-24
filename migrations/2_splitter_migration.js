a27827f2334f0cafca4a89feb340143cc5677b40
CallMeGwei <callmegwei@gmail.com>
2019-04-23 21:26:53 -0500

    Remove Ownable from truffle migrations.

--- migrations/2_splitter_migration.js
+++ migrations/2_splitter_migration.js
@@ -3,8 +3,6 @@ const Ownable = artifacts.require("Ownable");
 
 module.exports = function(deployer) {
   
-  deployer.deploy(Ownable);
-  deployer.link(Ownable, Splitter);
   deployer.deploy(Splitter);
   
 };
